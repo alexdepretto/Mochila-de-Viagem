e8e0fbbe188b82a4fd669c7cd69b4bd8143aab9e
Alex De Pretto <102765220+alexdepretto@users.noreply.github.com>
2022-09-16 10:43:52 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mochila de VIagem
+
+(Português / Portuguese)
+
+Página Web dinâmica focando em armazenamento de dados em localSotrage utilizando JavaAcript
+
+
+(Inglês / English)
+
+Dynamic web page focusing in data storage in localStorage using JavaScript.
---
+++ js/main.js
@@ -0,0 +1,68 @@
+
+const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+itens.forEach( (item) => {
+  criarElemento(item);
+});
+
+form.addEventListener('submit', (evento) => {
+  evento.preventDefault();
+
+  const nome = evento.target.elements['nome'];
+  const quantidade = evento.target.elements['quantidade'];
+  const existe = itens.find( (item) => item.nome === nome.value);
+  const itemAtual = {
+    'nome': nome.value,
+    'quantidade': quantidade.value
+  };
+  
+  if (existe) {
+    itemAtual.id = existe.id;
+    atualizarElemento(itemAtual);
+    itens[itens.findIndex((item) => item.id === existe.id)] = itemAtual;
+  }
+  else {
+    itemAtual.id = itens[itens.length - 1] ? (itens[itens.length - 1]).id + 1 : 0;
+    criarElemento(itemAtual);
+    itens.push(itemAtual);
+  };
+  
+  localStorage.setItem('itens', JSON.stringify(itens));
+  nome.value = '';
+  quantidade.value = '';
+});
+
+function criarElemento(item) {
+  const novoItem = document.createElement('li');
+  novoItem.classList.add('item');
+
+  const numeroItem = document.createElement('strong');
+  numeroItem.innerHTML = item.quantidade;
+  numeroItem.dataset.id = item.id;
+
+  novoItem.appendChild(numeroItem);
+  novoItem.innerHTML += item.nome;
+  novoItem.appendChild(botao(item.id));
+  lista.appendChild(novoItem);
+}
+
+function atualizarElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
+};
+
+function botao(id) {
+  const botao = document.createElement('button');
+  botao.innerText = 'X';
+  botao.addEventListener('click', function() {
+    deletarElemento(this.parentNode, id);
+  });
+  return botao;
+};
+
+function deletarElemento(tag, id) {
+  tag.remove();
+  itens.splice(itens.findIndex((item) => item.id === id), 1);
+  localStorage.setItem('itens', JSON.stringify(itens));
+};
